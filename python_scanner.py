51aa0813ad37adb2d2388af199afaa26ee0555e3
Muhammad Navaid Zafar Ansari <123810418+navaidzansari@users.noreply.github.com>
2023-02-25 21:16:45 +0300

    Update python_scanner.py

--- python_scanner.py
+++ python_scanner.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
 
-
 # Take user input
 target = input("Enter target IP/Subnet: ")
 
@@ -29,37 +28,42 @@ for host in open_hosts:
     subprocess.check_output(nmap_command, shell=True)
 
 # Run Nuclei on last Nmap result
-last_nmap_scan = f"{open_hosts[-1]}_nmap_scan.txt"
-nuclei_templates_dir = "/home/kali/nuclei-templates"
-nuclei_command = f"nuclei -silent -u http://{open_hosts[-1]} -t {nuclei_templates_dir} -severity high,medium"
-print("Running Nuclei full scan...")
-subprocess.check_output(nuclei_command, shell=True)
+if open_hosts:
+    last_nmap_scan = f"{open_hosts[-1]}_nmap_scan.txt"
+    nuclei_templates_dir = "/home/kali/nuclei-templates"
+    nuclei_command = f"nuclei -silent -u http://{open_hosts[-1]} -t {nuclei_templates_dir} -severity high,medium"
+    print("Running Nuclei full scan...")
+    subprocess.check_output(nuclei_command, shell=True)
 
 # Run Dirb scan on last Nmap result
-dirb_command = f"dirb http://{open_hosts[-1]} -r -o dirb_scan.txt /usr/share/dirb/wordlists/common.txt"
-print("Running Dirb scan...")
-subprocess.check_output(dirb_command, shell=True)
+if open_hosts:
+    dirb_command = f"dirb http://{open_hosts[-1]} -r -o dirb_scan.txt /usr/share/dirb/wordlists/common.txt"
+    print("Running Dirb scan...")
+    subprocess.check_output(dirb_command, shell=True)
 
-# Search for SQLi and XSS vulnerabilities in Dirb scan results
-print("Searching for SQLi and XSS vulnerabilities in Dirb scan results...")
-with open('dirb_scan.txt', 'r') as dirb_scan_file:
-    for line in dirb_scan_file:
-        if line.startswith('+ '):
-            url_path = re.search(r'\+ (\S+)', line).group(1)
-            full_url = urljoin(f'http://{open_hosts[-1]}', url_path)
-            response = requests.get(full_url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # Check for SQL injection via UNION SELECT
-            if re.search(r"\bSELECT\b.+?\bUNION\b", soup.text):
-                print(f"Possible SQLi vulnerability found in {full_url}")
-            # Check for SQL injection via error messages
-            if "error in your SQL syntax" in soup.text:
-                print(f"Possible SQLi vulnerability found in {full_url}")
-            # Check for XSS via script tags
-            if "<script>" in soup.text.lower():
-                print(f"Possible XSS vulnerability found in {full_url}")
-            # Check for XSS via href attributes
-            if 'javascript:' in soup.text.lower():
-                print(f"Possible XSS vulnerability found in {full_url}")
+# Run Nikto scan on last Nmap result
+if open_hosts:
+    nikto_command = f"nikto -h {open_hosts[-1]} -o nikto_scan.txt"
+    print("Running Nikto scan...")
+    subprocess.check_output(nikto_command, shell=True)
+
+# Search for SQLi and XSS vulnerabilities in Dirb and Nikto scan results
+if open_hosts:
+    print("Searching for SQLi and XSS vulnerabilities in Dirb and Nikto scan results...")
+    with open('dirb_scan.txt', 'r') as dirb_scan_file:
+        for line in dirb_scan_file:
+            if line.startswith('+ '):
+                url_path = re.search(r'\+ (\S+)', line).group(1)
+                full_url = urljoin(f'http://{open_hosts[-1]}', url_path)
+                response = requests.get(full_url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                if "sql" in soup.text.lower():
+                    print(f"SQLi vulnerability found in {full_url}")
+                if "<script>" in soup.text.lower():
+                    print(f"XSS vulnerability found in {full_url}")
+    with open('nikto_scan.txt', 'r') as nikto_scan_file:
+        for line in nikto_scan_file:
+        if "OSVDB" in line:
+            print(f"Vulnerability found in Nikto scan: {line.strip()}")
 
 print("Done.")
